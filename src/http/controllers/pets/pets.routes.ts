58cf6100941db16ceb32454d6fceb5ffb79c6386
Leonardo <leonardo.everton@inatel.br>
2025-03-27 20:18:22 -0300

    feat: get pet controller

--- src/http/controllers/pets/pets.routes.ts
+++ src/http/controllers/pets/pets.routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { createPetController } from './create-pet.controller'
+import { getPetController } from './get-pet.controller'
+
 
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', { onRequest: [verifyJwt] }, createPetController)
-
+  app.get('/orgs/pets/:id', getPetController)
 }
